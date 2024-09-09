6b3ccabdd9d92fa8226dbc092e29e69566c1098b
jeyoung5088 <jeyoung5088@gmail.com>
2024-09-09 20:49:10 +0900

    ~3.9.3

--- next-sample/pages/api/hello.ts
+++ next-sample/pages/api/hello.ts
@@ -1,13 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 
-type Data = {
+type HelloResponse = {
   name: string;
-};
+}
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>,
-) {
-  res.status(200).json({ name: "John Doe" });
+// /api/hello에서 호출될 때의 API 작동을 구현
+export default (req: NextApiRequest, res: NextApiResponse<HelloResponse>) => {
+  // 상태 200으로 {"name": "John Doe"}를 반환한다.
+  res.status(200).json({name: 'John Doe'})
 }
--- next-sample/pages/ssg.tsx
+++ next-sample/pages/ssg.tsx
@@ -1,30 +1,43 @@
-// 타입을 위해 도입
-import { NextPage } from 'next'
-// Next.js의 내장 컴폰넌트
-import Head from 'next/head'
+import { GetStaticProps, NextPage, NextPageContext } from "next";
+import Head from "next/head";
 
-// 페이지 컴포넌트의 props 타입 정의(여기에서는 빔)
-type SSGProps = {}
+// 페이지 컴포넌트인 props의 타입 정의
+type SSGProps = {
+    message: string;
+};
 
-// SSG용 페이지 구현
-// NextPage는 Next.js의 Pages용 타입
-// NextPage<props>에서 props가 들어가는 page임을 명시
-const SSG: NextPage<SSGProps> = () => {
+// SSG는 getStaticProps가 반환한 props를 받을 수 있음
+// NextPage<SSGProps>는 Message: string만을 받아 생성된 페이지 타입
+// next.js의 페이지 컴포넌트나 ㅎ마수 타입은 https://nextjs.org/learn/excel/typescript/nextjs-types 도 참고
+const SSG: NextPage<SSGProps> = (props) => {
+    const { message } = props;
+
     return (
         <div>
-            {/* Head 컴포넌트로 감싸면, 그 요소는 <head> 태그에 배치 */}
             <Head>
                 <title>Static Site Generation</title>
-                <link rel ="icon" href="/favicon.ico" />
+                <link rel="icon" href="/favicon.ico" />
             </Head>
             <main>
-                <p>
-                    이 페이지는 정적 사이트 생성을 통해 빌드 시 생성된 페이지입니다.
-                </p>
+                <p>이 페이지는 정적 사이트 생성을 통해 빌드 시 생성된 페이지</p>
+                <p>{message}</p>
             </main>
         </div>
     )
 }
 
-// 페이지 컴포넌트는 export default로 익스포트
-export default SSG
+// getStaticProps는 빌드 시 실행됨
+// GetStaticProps<SSGProps>는 SSGProps 인수로 받는 getStaticProps 타입
+export const getStaticProps: GetStaticProps<SSGProps> = async (context) =>{
+    const timestamp = new Date().toLocaleString();
+    const message = `${timestamp}에 getStaticProps가 실행되었음`;
+    console.log(message);
+    return {
+        // 여기에서 반환한 props를 기반으로 페이지 컴포넌트를 그림
+        props: {
+            message,
+        },
+    };
+};
+
+export default SSG;
