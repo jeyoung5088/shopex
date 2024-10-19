cd474a9c145442d837c0dcc2dd89b106e01a96dc
jeyoung5088 <jeyoung5088@gmail.com>
2024-10-19 21:07:35 +0900

    storybook onClick

--- next-sample/stories/StyledButton.stories.tsx
+++ next-sample/stories/StyledButton.stories.tsx
@@ -1,5 +1,8 @@
+import React, { useState } from 'react';
 import { Meta, StoryObj } from '@storybook/react';
 import { StyledButton } from '../components/StyledButton'
+// 새롭게 action을 임포트
+import { action } from '@storybook/addon-actions';
 
 // 파일 안의 스토리 설정(메타데이터 객체)
 export default {
@@ -12,12 +15,25 @@ export default {
 } as Meta<typeof StyledButton>;
 
 type Story = StoryObj<typeof StyledButton>;
+const incrementAction = action('increment');
+
+const PrimaryButton = () => {
+    const [count, setCount] = useState(0);
+    const onClick = (e: React.MouseEvent) => {
+        // 현재 카운트를 전달하고 Action을 호출
+        incrementAction(e, count);
+        setCount((c) => c+1);
+    }
+
+    return (
+        <StyledButton variant = "primary" onClick = {onClick}>
+            Count: {count}
+        </StyledButton>
+    );
+};
 
 export const Primary: Story = {
-    args: {
-        variant: 'primary',
-        children: 'Primary',
-    },
+    render: () => <PrimaryButton />,
 };
 
 export const Success: Story = {
